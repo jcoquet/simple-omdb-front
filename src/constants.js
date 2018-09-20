9fa1f559565cc8a33b70a7ebf6d6dc41f0cdd734
jcoquet <jbc_1984@hotmail.com>
2018-09-20 17:54:03 +0200

    Setting up some constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const API = '//www.omdbapi.com/';
+export const API_VERSION = 1;
+export const API_KEY = '596cff53';
+export const API_KEY_PARAM = 'apikey';
+export const API_SEARCH_PARAM = 's';
+export const API_PAGE_PARAM = 'page';
+export const MAX_BOOKMARKS = 3;

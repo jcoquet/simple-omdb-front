f8e2ac436ac12a6cdd01cacdeeac4165d2106dd4
jcoquet <jbc_1984@hotmail.com>
2018-09-23 23:14:35 +0200

    header / footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Loader from './Loader';
+import { Link } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <div className="layout">
+                <header>
+                    <Link to='/'>BlablaMovie</Link>
+                </header>
                 {this.props.children && React.cloneElement(this.props.children)}
+                <footer>
+                    2018 - <a target="_blank" href="https://github.com/jcoquet/simple-omdb-front">Github</a>
+                </footer>
                 <Loader />
             </div>
         );
